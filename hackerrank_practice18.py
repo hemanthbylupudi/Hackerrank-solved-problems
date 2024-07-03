f4f12abc030957edf9dbdb5116a025364a46d11d
hemanthbylupudi <122376842+hemanthbylupudi@users.noreply.github.com>
2024-07-03 13:36:24 +0530

    Add files via upload

---
+++ hackerrank_practice18.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+m=int(input())
+a=set(map(int,input().split()))
+n=int(input())
+b=set(map(int,input().split()))
+res=a.difference(b)
+print(len(res))
